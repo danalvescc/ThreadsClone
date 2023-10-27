38da0d82cbff223777968209984ebfa6fce7ea9b
Daniel Alves <danalvescc@gmail.com>
2023-10-26 22:27:49 -0300

    Initial Commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Daniel  Alves Barreto on 26/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
