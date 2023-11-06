0b7d048676a71f9c30e6350bab5c734fd61064a6
Daniel Alves <danalvescc@gmail.com>
2023-11-06 17:40:31 -0300

    feat: upload user to firestore

---
+++ Threads/Core/Authentication/Service/UserService.swift
@@ -0,0 +1,8 @@
+//
+//  UserService.swift
+//  Threads
+//
+//  Created by Daniel  Alves Barreto on 06/11/23.
+//
+
+import Foundation
---
+++ Threads/Core/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewModel.swift
+//  Threads
+//
+//  Created by Daniel  Alves Barreto on 06/11/23.
+//
+
+import Foundation
